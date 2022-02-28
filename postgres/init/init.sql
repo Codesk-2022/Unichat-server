f618b09ddcef9ebd0be73923c5178a6298605b24
ikasoba <57828948+ikasoba@users.noreply.github.com>
2022-02-28 22:00:43 +0900

    fix init.sql

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,13 +1,13 @@
 CREATE EXTENSION pgcrypto;
 
 CREATE TABLE users (
-  "id"         UUID    NOT NULL,
-  "name"       varchar(30) NOT NULL,
-  "owner"      UUID    NOT NULL,
-  "categories" UUID[],
-  "emojis"     UUID[],
-  "stamps"     UUID[],
-  "created_at" timestamp NOT NULL
+  "id"            UUID    NOT NULL,
+  "email"         varchar(256) NOT NULL,
+  "username"      varchar(30) NOT NULL,
+  "password"      varchar(30) NOT NULL,
+  "tag"           smallint    NOT NULL,
+  "registered_at" timestamp NOT NULL,
+  "verified"      boolean NOT NULL
 );
 
 CREATE TABLE servers (
@@ -23,29 +23,26 @@ CREATE TABLE servers (
 CREATE TABLE categories (
   "id"         UUID    NOT NULL,
   "name"       varchar(30) NOT NULL,
-  "owner"      UUID    NOT NULL,
-  "categories" UUID[],
-  "emojis"     UUID[],
-  "stamps"     UUID[],
+  "server"     UUID NOT NULL,
+  "channels"   UUID[],
   "created_at" timestamp NOT NULL
 );
 
 CREATE TABLE channels (
-  "id"         UUID    NOT NULL,
-  "name"       varchar(30) NOT NULL,
-  "owner"      UUID    NOT NULL,
-  "categories" UUID[],
-  "emojis"     UUID[],
-  "stamps"     UUID[],
-  "created_at" timestamp NOT NULL
+  "id"          UUID    NOT NULL,
+  "name"        varchar(30) NOT NULL,
+  "description" varchar(120),
+  "server"      UUID NOT NULL,
+  "category"    UUID NOT NULL,
+  "created_at"  timestamp NOT NULL
 );
 
 CREATE TABLE messages (
   "id"         UUID    NOT NULL,
-  "name"       varchar(30) NOT NULL,
-  "owner"      UUID    NOT NULL,
-  "categories" UUID[],
-  "emojis"     UUID[],
-  "stamps"     UUID[],
+  "user"       UUID NOT NULL,
+  "content"    varchar(300) NOT NULL,
+  "server"     UUID NOT NULL,
+  "channel"    UUID NOT NULL,
   "created_at" timestamp NOT NULL
 );
+
